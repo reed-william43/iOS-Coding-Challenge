5b1dcc421c290ae24d89e40baba435b66e1c1a9f
William Reed <williamreed995@gmail.com>
2023-11-03 09:11:50 -0500

    Initial Commit

---
+++ iOS_take_home/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS_take_home
+//
+//  Created by William Reed on 11/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS_take_home/iOS_take_homeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_take_homeApp.swift
+//  iOS_take_home
+//
+//  Created by William Reed on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS_take_homeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
